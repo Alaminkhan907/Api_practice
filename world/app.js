4bccdffb2587a3d362d0097b948012351a487333
mdkhaz <mdkhaz@ttu.ee>
2021-02-11 15:34:12 +0200

    finish

--- world/app.js
+++ world/app.js
@@ -9,14 +9,33 @@ const displayCountries = countries =>{
 
     const countriesDiv = document.getElementById('courntries');
     countries.forEach(country => {
-        console.log(country);
         const countryDiv = document.createElement('div');
         countryDiv.className ='country';
         const countryInfor =`
         <h3 class ="country-name">${country.name}</h3>
         <p>${country.capital}</p>
+        <button onclick ="displayCountryDetails('${country.name}')">Details</button>
         `
         countryDiv.innerHTML=countryInfor;
         countriesDiv.appendChild(countryDiv);
     });
+}
+
+const displayCountryDetails = name =>{
+    const url = `https://restcountries.eu/rest/v2/name/${name}`
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>{
+        renderCountryInfo(data[0]);
+    })
+}
+const renderCountryInfo = country =>{
+    console.log(country);
+    const countryDiv =document.getElementById('countryDetails');
+    countryDiv.innerHTML =`
+    <h1>${country.name}</h1>
+    <p>Population :${country.population}</p>
+    <p>Area :${country.area}</p>
+    <img src="${country.flag}">
+    `
 }
