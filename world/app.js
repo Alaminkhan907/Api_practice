8dc4364c95f9df7715fd4554745907d1607a7e01
mdkhaz <mdkhaz@ttu.ee>
2021-02-11 15:06:09 +0200

    foreach

--- world/app.js
+++ world/app.js
@@ -8,9 +8,8 @@ fetch("https://restcountries.eu/rest/v2/all")
 const displayCountries = countries =>{
 
     const countriesDiv = document.getElementById('courntries');
-    for (let i = 0; i < countries.length; i++) {
-        const country = countries[i];
-        //console.log(country.name);
+    countries.forEach(country => {
+        console.log(country);
         const countryDiv = document.createElement('div');
         countryDiv.className ='country';
         const countryInfor =`
@@ -19,7 +18,5 @@ const displayCountries = countries =>{
         `
         countryDiv.innerHTML=countryInfor;
         countriesDiv.appendChild(countryDiv);
-        
-     
-    }
+    });
 }
